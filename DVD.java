6b5bbfeaf4da4951050d91c1de12a2b8aa6dd194
JamieJY <98108262+JamieJY@users.noreply.github.com>
2023-07-10 14:08:43 -0400

    ArrayList CD+DVD

--- DVD.java
+++ DVD.java
@@ -1,36 +1,39 @@
 package dome;
 
-public class CD {
+public class DVD {
 	private String title;
-	private String artist;
-	private int numofTracks;
+	private String director;
+	
 	private int playingTime;
 	private boolean gotIt=false;
 	private String comment;
+	
+	
+	
+	
 
-	
-	
-	
-	
-	public CD(String title, String artist, int numofTracks, int playingTime, String comment) {
-	//	super();
+
+
+	public DVD(String title, String director, int playingTime, String comment) {
+		super();
 		this.title = title;
-		this.artist = artist;
-		this.numofTracks = numofTracks;
+		this.director = director;
 		this.playingTime = playingTime;
 		this.comment = comment;
 	}
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+
+
 	public void print() {
 		// TODO Auto-generated method stub
-		System.out.println("CD:"+title+":"+artist);
+		System.out.println("DVD:"+title+":"+director);
 	}
-
-	
 
 }
--- Database.java
+++ Database.java
@@ -4,24 +4,37 @@ import java.util.ArrayList;
 
 public class Database {
 	private ArrayList<CD> listCD = new ArrayList<CD>();
+	private ArrayList<DVD> listDVD = new ArrayList<DVD>();
+	
 	
 	public void add(CD cd) {
 		listCD.add(cd);
+	}
+	
+	public void add(DVD dvd) {
+		listDVD.add(dvd);
 	}
 	
 	public void list() {
 		for(CD cd:listCD) {
 			cd.print();
 		}
+
+
+		for(DVD dvd:listDVD) {
+			dvd.print();
+		}
 	}
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Database db = new Database();
 		db.add(new CD("abc","abc",3,60,"..."));
-		db.add(new CD("abc","abc",3,60,"..."));
+		db.add(new CD("def","def",3,60,"..."));
+		
+		db.add(new DVD("ghi","ghi",100,"..."));
+		db.add(new DVD("xxx","ghi",100,"..."));
+		
 		db.list();
-	
 	}
 
 }
