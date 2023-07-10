fb720bf5f5df5aa1d423dc0f09c4d449f63f7e51
JamieJY <98108262+JamieJY@users.noreply.github.com>
2023-07-10 12:18:45 -0400

    HashMap input string output amount

--- Coin.java
+++ Coin.java
@@ -1,33 +1,45 @@
 package coins;
 
 import java.util.HashMap;
+
 import java.util.Scanner;
 
 public class Coin {
-	private HashMap<Integer,String> coinnames = new HashMap<Integer,String>();
+	private HashMap<String,Integer> coinnames = new HashMap<String,Integer>();
 	
 	//iniciallize 
 	
 	public Coin(){
 		//coinnames.put(key, value);
-		coinnames.put(1, "penny");
-		coinnames.put(10, "dime");
-		coinnames.put(25, "quarter");
-		coinnames.put(50, "half-dollar");
+		coinnames.put("penny",1);
+		coinnames.put("dime",10);
+		coinnames.put("quarter",25);
+		coinnames.put("half-dollar",50);
 		
 	}
 	
 	
-	public String getName(int amount) {
-		return coinnames.get(amount);
-	}
+//	public String getName(int amount) {
+//		return coinnames.get(amount);
+//	}
+	public Integer getName(String k) {
+ 	   return coinnames.get(k);}
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		Scanner in = new Scanner(System.in);
+//		int amount = in.nextInt();
+//		Coin coin=new Coin();
+//		String name = coin.getName(amount);
+//		System.out.println(name);
 		Scanner in = new Scanner(System.in);
-		int amount = in.nextInt();
+		String s=in.next();
+	
+		
 		Coin coin=new Coin();
-		String name = coin.getName(amount);
-		System.out.println(name);
+	   
+	    	Integer value=coin.getName(s);
+		System.out.println(value);
 		
 		
 	}
