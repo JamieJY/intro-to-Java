7d612508e2ac5c677a0d90041f68d2df5490a3ea
JamieJY <98108262+JamieJY@users.noreply.github.com>
2023-07-10 16:07:15 -0400

    reduce redundancy

--- CD.java
+++ CD.java
@@ -1,24 +1,22 @@
 package dome;
 
 public class CD extends Item {
-	private String title;
+	
 	private String artist;
 	private int numofTracks;
-	private int playingTime;
-	private boolean gotIt=false;
-	private String comment;
+
 
 	
 	
 	
 	
 	public CD(String title, String artist, int numofTracks, int playingTime, String comment) {
-	//	super();
-		this.title = title;
+        super(title,playingTime,false,comment);
+		//this.title = title;
 		this.artist = artist;
 		this.numofTracks = numofTracks;
-		this.playingTime = playingTime;
-		this.comment = comment;
+		//this.playingTime = playingTime;
+		//this.comment = comment;
 	}
 
 	public static void main(String[] args) {
--- DVD.java
+++ DVD.java
@@ -1,12 +1,12 @@
 package dome;
 
 public class DVD extends Item {
-	private String title;
+	//private String title;
 	private String director;
 	
-	private int playingTime;
-	private boolean gotIt=false;
-	private String comment;
+//	private int playingTime;
+	//private boolean gotIt=false;
+//	private String comment;
 	
 	
 	
@@ -15,25 +15,29 @@ public class DVD extends Item {
 
 
 	public DVD(String title, String director, int playingTime, String comment) {
-		super();
-		this.title = title;
+		super(title,playingTime,false,comment);
+		//this.title = title;
 		this.director = director;
-		this.playingTime = playingTime;
-		this.comment = comment;
+		//this.playingTime = playingTime;
+		//this.comment = comment;
 	}
 
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DVD dvd = new DVD("a","b",1,"..");
+		dvd.print();
 	}
 
 
 
 	public void print() {
 		// TODO Auto-generated method stub
-		System.out.println("DVD:"+title+","+director);
+		//System.out.println("DVD:"+title+","+director);
+		System.out.print("DVD:");
+		super.print();
+		System.out.print(director);
 	}
 
 }
